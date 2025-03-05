af74b224611d2035772b1048581552693ea7139f
KAPTOH2 <Muratkini1998@mail.ru>
2025-03-06 02:43:01 +0300

    Delete sait directory

--- sait/sait_1/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class StudentForm(forms.Form):
-    names = forms.CharField(label="Имена учеников", widget=forms.Textarea(attrs={'rows': 4}))
-    grades = forms.CharField(label="Оценки", widget=forms.Textarea(attrs={'rows': 4}))
